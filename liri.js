bc1304d9f1f8c402f89071a9caf9e35d6c1052f4
Joy Holowicki <joyholowicki@gmail.com>
2016-12-17 01:08:40 -0600

    initial

---
+++ liri.js
@@ -0,0 +1,15 @@
+// At the top of the liri.js file, 
+// write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+
+
+
+
+// * `my-tweets`
+
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
